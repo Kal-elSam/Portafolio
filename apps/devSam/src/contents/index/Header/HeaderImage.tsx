dbb6df05513d8cf000840fa734263f48b6364e34
Sam <samgomezs7@hotmail.com>
2023-07-12 08:33:03 -0600

    ⚡️ 1

--- apps/devSam/src/contents/index/Header/HeaderImage.tsx
+++ apps/devSam/src/contents/index/Header/HeaderImage.tsx
@@ -10,19 +10,19 @@ function HeaderImage() {
 
   return (
     <div
-      className={clsx('relative h-[590px] w-[603px]')}
-      style={{
-        maskImage: `url("data:image/svg+xml,%3Csvg width='603' height='590' fill='none' viewBox='0 0 603 590' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='m0 0v393h228v9.5c0 103.55 83.947 187.5 187.5 187.5s187.5-83.947 187.5-187.5v-402.5h-603z' fill='%23000'/%3E%3C/svg%3E%0A")`,
-        WebkitMaskImage: `url("data:image/svg+xml,%3Csvg width='603' height='590' fill='none' viewBox='0 0 603 590' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='m0 0v393h228v9.5c0 103.55 83.947 187.5 187.5 187.5s187.5-83.947 187.5-187.5v-402.5h-603z' fill='%23000'/%3E%3C/svg%3E%0A")`,
-      }}
+      className={clsx('relative h-[590px] w-[703px]')}
+      // style={{
+      //   maskImage: `url("data:image/svg+xml,%3Csvg width='903' height='590' fill='none' viewBox='0 0 603 590' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='m0 0v393h228v9.5c0 103.55 83.947 187.5 187.5 187.5s187.5-83.947 187.5-187.5v-402.5h-603z' fill='%23000'/%3E%3C/svg%3E%0A")`,
+      //   WebkitMaskImage: `url("data:image/svg+xml,%3Csvg width='603' height='590' fill='none' viewBox='0 0 603 590' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='m0 0v393h228v9.5c0 103.55 83.947 187.5 187.5 187.5s187.5-83.947 187.5-187.5v-402.5h-603z' fill='%23000'/%3E%3C/svg%3E%0A")`,
+      // }}
     >
       <div
         className={clsx(
           'from-accent-400/20 via-accent-400/0 absolute top-0 right-0 h-[590px] w-[375px] rounded-full bg-gradient-to-t',
-          'dark:from-accent-600/10 dark:via-accent-600/0'
+          'dark:from-accent-600/10 dark:via-accent-600/0','flex justify-center items-center' 
         )}
       >
-        <div className={clsx('absolute right-0 bottom-0 overflow-hidden')}>
+        <div className={clsx('absolute right-90 bottom-100 overflow-hidden')}>
           <m.div
             className={clsx('absolute z-[10]')}
             initial={{ opacity: 1 }}
@@ -53,10 +53,10 @@ function HeaderImage() {
             animate={controlsHeaderImage}
           >
             <Image
-              alt="Enji Kusnadi Illustration"
-              src="/assets/images/me.png"
-              width={457}
-              height={526}
+              alt="Sam Gomez"
+              src="/assets/images/sam.png"
+              width={1057}
+              height={783}
               className={clsx(
                 'hidden max-w-none',
                 'lg:block',
